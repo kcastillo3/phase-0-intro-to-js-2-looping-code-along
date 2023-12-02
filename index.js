cdef57b2a61ca1aa1c9b02a5b6bf157864b6c53a
Kevin <kevincastillo333@gmail.com>
2023-12-02 13:59:48 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solutions in this file
+// Example 1: for loop
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+  
+  // Example 2: for loop with arrays
+  const gifts = ["teddy bear", "drone", "doll"];
+  
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  }
+  
+  wrapGifts(gifts);
+  
+  // Example 3: while loop
+  let count = 10;
+  while (count >= 0) {
+    console.log(count);
+    count--;
+  }
+  
+  // Example 4: while loop with a condition
+  let keepWorking = true;
+  while (keepWorking) {
+    chooseSeedLocation();
+    plantSeed();
+    waterSeed();
+    keepWorking = checkForMoreSeeds();
+  }
+  
+  // Example 5: function using a while loop
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  countDown(10);
+  
+  // New function: writeCards
+  function writeCards(names) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
